244c35cf2cc7e23c913a96ebac38e2b913ec79c5
Masaki-ohara <masakingu921@gmail.com>
2023-05-14 22:29:02 +0900

    プライバシーポリシー作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+  get 'privacy_policy', to: 'static_pages#privacy_policy'
   resources :users, only: %i(new create show)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resource :players, only: %i[show] do
