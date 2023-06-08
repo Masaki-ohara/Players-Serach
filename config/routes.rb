4c83bf570097f22ad7b2fc6bcfd3dcb5ffca85d4
Masaki-ohara <masakingu921@gmail.com>
2023-06-08 09:53:35 +0900

    topページのlayout変更

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
   get 'privacy_policy', to: 'static_pages#privacy_policy'
+  get 'terms_of_use', to: 'static_pages#terms_of_use'
+  get 'treatment', to: 'players#treatment'
   resources :users, only: %i(new create show)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resource :players, only: %i[show] do
