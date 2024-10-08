16424033cf349a44572ee08e89c25487c3de94c1
Masaki-Ohara <masakingu921@gmail.com>
2024-10-09 00:49:40 +0900

    デプロイ準備

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,10 @@
-render-build.sh#!/usr/bin/env bash
+#!/usr/bin/env bash
 # exit on error
 set -o errexit
 
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
+yarn install
+yarn build # jsファイルをesbuildでバンドルしているため
+bundle exec rake assets:precompile # cssはsprocketsを使っているため
+bundle exec ridgepole -c config/database.yml -E production --apply -f db/schemas/Schemafile # migrateはridgepoleを使っているため（標準のmigrateを使うならbundle exec rails db:migrateで良いかと思います）
+
